f90eda0526906077568e2c1e051a50332e6aae66
ValentinCaribou <valentinbdu79@gmail.com>
2019-10-04 16:37:27 +0200

    Ajout des du système de route et de la page 404

---
+++ src/pages/error/error404.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './error404.css';
+
+class Error404 extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+
+            <div>
+                <div className="body-container">
+                    <div className="white-container">
+                        <h1 className="error-title">404</h1>
+                        <span className="error-text">Vous êtes sûr d'être au bon endroit ?</span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Error404;
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,25 +1,22 @@
 import React, {Component}  from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from '../logo.svg';
-import '../App.css';
+import '../App.scss';
+import Error404 from "../pages/error/error404"
+import Home from "../pages/home/home"
 
 export default class Main extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+                <main>
+                    <Router>
+                        <Switch>
+                            <Route exact path="/" component={Home}/>
+                            <Route component={Error404}/>
+                        </Switch>
+                    </Router>
+                </main>
             </div>
         );
     }
