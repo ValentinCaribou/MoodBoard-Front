7ce8e785c64fe50ab4438892fcb4ba2fd9bedb07
ValentinCaribou <valentinbdu79@gmail.com>
2019-10-04 15:42:36 +0200

    Passage au format jsx + ajout des composants et des pages

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, {Component}  from 'react';
+import Main from './pages/main.jsx'
+import './App.css';
+
+export default class App extends Component {
+    render() {
+        return (
+            <Main/>
+        );
+    }
+};
