4736aaf2f63bb75ee7bb156ea8c981345ce6e910
clemdavid <clement_david@live.fr>
2019-10-07 14:54:57 +0200

    added moodboard folder + page added components folder added week component file

---
+++ src/pages/moodboard/moodboard.jsx
@@ -0,0 +1,8 @@
+import React, {Component}  from 'react';
+import '../../App.scss';
+
+export default class MoodBoard extends Component{
+    render(){
+        <h1>Here is the component page.</h1>
+    }
+}
