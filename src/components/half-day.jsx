e9de44c0e12c9c40a69a0dd6709a121d5571f1e3
clemdavid <clement_david@live.fr>
2019-10-08 10:51:23 +0200

    init of component added link between components

---
+++ src/components/half-day.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class HalfDay extends Component{
+    render(){
+        return (
+        <div>
+            <h3>HalfDay Component</h3>
+        </div>
+        ); 
+    }
+}
