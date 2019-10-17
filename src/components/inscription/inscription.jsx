e9080ff247dc24c88dbaeccc65830672e3181855
Valentin <valentinbdu79@gmail.com>
2019-10-17 12:01:28 +0200

    Formulaire d'inscription avec mis en place la vérif coté mail et mot de passe

--- src/components/inscription/inscription.jsx
+++ src/components/inscription/inscription.jsx
@@ -5,9 +5,54 @@ export default class Week extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            user : {
+                adresseMail:"",
+                password:"",
+                confirmePassword:"",
+                nom:"",
+                prenom:""
+            },
+            isError: false,
+            errorMessage: ""
+        }
     }
 
+    handleOnChange = (e) => {
+        const target = e.currentTarget;
+        this.persistUser(target.id, target.value);
+    };
+
+    persistUser = (name, value) => {
+        const user = {...this.state.user};
+        user[name] = value;
+        this.setState({user});
+    };
+
+    validateEmail(email) {
+        const regexMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regexMail.test(String(email).toLowerCase());
+    }
+
+    validateInscription = () => {
+        const user = this.state.user;
+        let valide = this.validateEmail(user.adresseMail);
+        console.log(valide);
+        if (valide){
+            if(user.password !== user.confirmePassword){
+                this.setState({errorMessage: "Les mots de passe ne corresponde pas"});
+                this.setState({isError: true});
+            }
+            console.log(this.state.user);
+        } else {
+            console.log("test");
+            this.setState({errorMessage: "Adresse mail non valide"});
+            this.setState({isError: true});
+        }
+    };
+
     render() {
+        const {user, isError, errorMessage} = this.state;
         return (
             <div id="myModal" className="modal">
                 <div className="modal-content">
@@ -16,32 +61,38 @@ export default class Week extends Component {
                             <div className="div-close">
                                 <span className="close" onClick={this.props.changeStatus}>&times;</span>
                             </div>
+                            {
+                                isError &&
+                                <div className="group">
+                                    <label>{errorMessage}</label>
+                                </div>
+                            }
                             <div className="group">
-                                <input type="text" id="adresseMail" className="inputText" required="required"/><label
-                                htmlFor="adresseMail">Adresse mail</label>
+                                <input type="text" id="adresseMail" className="inputText" required="required" onChange={this.handleOnChange} value={user.adresseMail}/>
+                                <label htmlFor="adresseMail">Adresse mail</label>
                                 <div className="bar"></div>
                             </div>
                             <div className="group">
-                                <input type="password" id="password" className="inputText" required="required" onChange={this.handleOnChange}></input>
+                                <input type="password" id="password" className="inputText" required="required" onChange={this.handleOnChange} value={user.password}/>
                                 <label htmlFor="password">Mot de passe</label>
                                 <div className="bar"></div>
                             </div>
                             <div className="group">
-                                <input type="password" id="confirmePassword" className="inputText" required="required" onChange={this.handleOnChange}></input>
+                                <input type="password" id="confirmePassword" className="inputText" required="required" onChange={this.handleOnChange} value={user.confirmePassword}/>
                                 <label htmlFor="confirmePassword">Confirmer le mot de passe : </label>
                                 <div className="bar"></div>
                             </div>
                             <div className="group">
-                                <input type="text" id="nom" className="inputText" required="required" onChange={this.handleOnChange}></input>
+                                <input type="text" id="nom" className="inputText" required="required" onChange={this.handleOnChange} value={user.nom}/>
                                 <label htmlFor="nom">Nom : </label>
                                 <div className="bar"></div>
                             </div>
                             <div className="group">
-                                <input type="text" id="prenom" className="inputText" required="required" onChange={this.handleOnChange}></input>
+                                <input type="text" id="prenom" className="inputText" required="required" onChange={this.handleOnChange} value={user.prenom}/>
                                 <label htmlFor="prenom">Prenom : </label>
                                 <div className="bar"></div>
                             </div>
-                            <input type="submit" className="validate-button" value="Créer le compte" onClick={this.changeHide}/>
+                            <input type="submit" className="validate-button" value="Créer le compte" onClick={this.validateInscription}/>
                         </div>
                     </div>
                 </div>
